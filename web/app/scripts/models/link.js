f6f28b03a2da170460f60007ef7397ca643b7639
Guten Ye <ywzhaifei@gmail.com>
2015-02-18 18:17:34 +0800

    web2 -> web

--- web/app/scripts/models/link.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-angular.module('App').factory('Link', ['resource', function($resource) {
-  return $resource('/api/links/:id', {id: '@id'})
-}])
--- web2/src/app/lib.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-_.isBlank = function(value) {
-  return _.isNull(value) || _.isUndefined(value) || (_.isString(value) && value.trim() === '')
-}
